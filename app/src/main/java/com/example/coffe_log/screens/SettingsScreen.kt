c93e02a3ef56d302977c0356f00909a6c2e8caf0
janezb10 <janezb10@gmail.com>
2024-01-11 00:25:43 +0100

    Alert dialog when deleting data

--- app/src/main/java/com/example/coffe_log/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/coffe_log/screens/SettingsScreen.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -21,10 +24,13 @@ fun SettingsScreen(
     onClickLogout: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val dialogOpen = remember { mutableStateOf(false) }
+
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Button(
-                onClick = {vm.deleteData()},
+//                onClick = {vm.deleteData()},
+                onClick = {dialogOpen.value = true},
                 modifier = Modifier
                     .padding(bottom = 20.dp)
             ) {
@@ -43,6 +49,32 @@ fun SettingsScreen(
                     fontSize = 30.sp
                 )
             }
+
+            if (dialogOpen.value) {
+                AlertDialog(
+                    onDismissRequest = { dialogOpen.value = false },
+                    title = { Text(text = "Delete Data") },
+                    text = { Text(text = "Are you sure you want to delete all data?") },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                vm.deleteData()
+                                dialogOpen.value = false
+                            }
+                        ) {
+                            Text(text = "Yes")
+                        }
+                    },
+                    dismissButton = {
+                        Button(
+                            onClick = { dialogOpen.value = false }
+                        ) {
+                            Text(text = "No")
+                        }
+                    }
+                )
+            }
+
         }
     }
 
